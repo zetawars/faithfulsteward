24c821c8eaefebd0ca27ec1a4e88ca4890b55163
Zawar <zetawars@hotmail.com>
2018-10-20 10:51:34 -0700

    update database

--- resources/views/Panel/Account/Login.blade.php
+++ resources/views/Panel/Account/Login.blade.php
@@ -40,7 +40,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
     <div class="login-logo">
-        <a href="../../index2.html"><b>Faithful</b>Steward</a>
+        <a href="#"><b>Faithful</b>Steward</a>
     </div>
     <!-- /.login-logo -->
     <div class="login-box-body">
